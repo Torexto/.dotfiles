da1c184728e4a1df77b921c54fcd6db19b47d60e
torexto <tomasztrela8@gmail.com>
2025-08-03 17:56:10 +0200

    install fix

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/sh
-sudo pacman -S kitty nvim hyprland gdm hypridle hyprlock fish waybar wofi swaync
+sudo pacman -S kitty nvim hyprland gdm hypridle hyprlock fish waybar wofi swaync ttf-jetbrains-mono-nerd grim swww
 yay -S wlogout pywal
