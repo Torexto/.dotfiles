75fc2c28c39807b8dd2275efce57ad99e2b50dc0
Torexto <tomasztrela8@gmail.com>
2025-09-11 13:45:21 +0200

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,27 @@
+source ~/.cache/wal/colors.fish
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Variables
 set -Ux EDITOR nvim
-# set -g fish_color_command normal
-# set -g fish_color_param gray
-# set -g fish_color_error gray
+set -U fish_user_paths ~/.dotnet/ $fish_user_paths
+set -U fish_user_paths ~/.sdk/Android/platform-tools/ $fish_user_paths
+set -U fish_user_paths ~/.sdk/Android/emulator/ $fish_user_paths
+
+# Aliases
+function fc
+  nvim ~/.config/fish/config.fish
+end
+
+function ff
+  fastfetch
+end
+
+# Executables
 starship init fish | source
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
